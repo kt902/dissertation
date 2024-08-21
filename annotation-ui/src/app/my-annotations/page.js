ac7dd3f2d15a6ffe0250b72cb1627c24c248faeb
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2024-08-21 11:56:53 +0100

    Add counts to my annotations

--- annotation-ui/src/app/my-annotations/page.js
+++ annotation-ui/src/app/my-annotations/page.js
@@ -10,12 +10,15 @@ export default async function MyAnnotationsServer() {
     const data = await datasets.getNarrations(annotations.map(a => a.narration_id));
     annotations = annotations.map(a => { return {...a, ...data[a.narration_id]}})
 
+    const allCount = annotations.length;
+    const completeCount = annotations.filter(a => a.status == 'complete').length;
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-center m-4">
             <div className="flex-grow w-full max-w-5xl">
                 <h2 className="text-2xl sm:text-3xl font-extrabold">My Annotations</h2>
                 <Suspense fallback={<div>Loading...</div>}>
-                    <MyAnnotations annotations={annotations} />
+                    <MyAnnotations annotations={annotations} allCount={allCount} completeCount={completeCount} />
                 </Suspense>
             </div>
         </main>
